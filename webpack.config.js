3cd30f17286ab211d0a0429896c5f0756e91490e
Prince Thind <princethind799@gmail.com>
2021-06-22 11:57:10 +0530

    initial setup

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+ const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+ module.exports = {
+   mode: 'development',
+   entry: {
+     index: './src/index.js',
+   },
+   plugins: [
+     new HtmlWebpackPlugin({
+       template:'./src/index.html',
+     }),
+   ],
+   devtool: 'inline-source-map',
+    module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+   output: {
+     filename: 'bundle.js',
+     path: path.resolve(__dirname, 'dist'),
+
+    clean: true,
+   },
+ };
