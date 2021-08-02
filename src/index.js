9e6a2314f25034ffe38650a6b20a571386c2b5fe
Prince-Thind <princethind799@gmail.com>
2021-08-02 18:36:32 +0530

    refactor code

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import './style.css';
 
-const weatherModule = (function () {
-  const submitButton = document.querySelector('#submit-button');
-  const userInput = document.querySelector('#input-city');
-  const cardContainer = document.querySelector('#card-container');
-  submitButton.addEventListener('click', main);
+(function () {
+  const form = document.querySelector('#user-input');
+  const mainDiv = document.querySelector('#main');
+  form.addEventListener('submit', main);
 
-  function main() {
-    const userCity = userInput.value.toLowerCase();
+  function main(e) {
+    e.preventDefault();
+    const userCity = form.city.value.toLowerCase();
     const APIkey = 'f2efb62c94d05d02a245c576e66225ef';
     const fetchURL = `https://api.openweathermap.org/data/2.5/weather?q=${userCity}&APPID=${APIkey}`;
 
@@ -20,33 +20,43 @@ const weatherModule = (function () {
       })
       .catch((err) => {
         console.log(err);
+        updateDisplay(null);
       });
   }
 
   function updateDisplay(data) {
-    function toCelsius(K) {
-      const k = 273.15;
-      return (K - 273.15).toFixed(1) + 'C';
+    let city = '-';
+    let summary = '-';
+    let present = '-';
+    let max = '-';
+    let min = '-';
+    let humidity = '-';
+    let feelsLike = '-';
+    if (data) {
+      city = data.name;
+      summary = data.weather[0].main;
+      present = toCelsius(data.main.temp);
+      max = toCelsius(data.main.temp_max);
+      min = toCelsius(data.main.temp_min);
+      humidity = data.main.humidity;
+      feelsLike = toCelsius(data.main.feels_like);
     }
 
-    const city = data.name;
-    const summary = data.weather[0].main;
-    const present = toCelsius(data.main.temp);
-    const max = toCelsius(data.main.temp_max);
-    const min = toCelsius(data.main.temp_min);
-    const humidity = data.main.humidity;
-    const feelsLike = toCelsius(data.main.feels_like);
-
-    cardContainer.innerHTML = `<div class="card">
-    <div class="city-name">${city}</div>
+    mainDiv.innerHTML = `  <div class="city-name">${city}</div>
     <div class="summary">${summary}</div>
-    <div class="present">Weather Now: ${present}</div>
-    <div class="min-max">
-      <div class="min">Max:${min}</div>
-      <div class="max">Min:${max}</div>
-    </div>
-    <div class="humidity">Humidity:${humidity}</div>
-    <div class="feels-like">Feels Like:${feelsLike}</div>
-  </div>`;
+    <div class="weather-info">
+        <div class="present">Weather Now: ${present}</div>
+        <div class="min-max">
+            <div class="min">Max: ${max}</div>
+            <div class="max">Min: ${min}</div>
+        </div>
+        <div class="humidity">Humidity: ${humidity}%</div>
+        <div class="feels-like">Feels Like: ${feelsLike}</div>
+    </div>`;
+  }
+
+  function toCelsius(K) {
+    const kelvinConstant = 273.15;
+    return (K - kelvinConstant).toFixed(1) + '&deg;' + 'C';
   }
 })();
