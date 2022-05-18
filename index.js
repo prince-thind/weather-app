8c22f4194a1f4d8b4d5776b97c110d41af5c9331
Prince Thind <Princethind799@gmail.com>
2022-05-18 10:32:07 +0530

    simplify JS

--- index.js
+++ index.js
@@ -1,46 +1,34 @@
-import './style.css';
+const form = document.querySelector("#user-input");
+const mainDiv = document.querySelector("#main");
+form.addEventListener("submit", main);
 
-(() => {
-  const form = document.querySelector('#user-input');
-  const mainDiv = document.querySelector('#main');
-  form.addEventListener('submit', main);
+function main(e) {
+  e.preventDefault();
+  const userCity = form.city.value.toLowerCase();
+  const APIkey = "f2efb62c94d05d02a245c576e66225ef";
+  const fetchURL = `https://api.openweathermap.org/data/2.5/weather?q=${userCity}&APPID=${APIkey}`;
 
-  function main(e) {
-    e.preventDefault();
-    const userCity = form.city.value.toLowerCase();
-    const APIkey = 'f2efb62c94d05d02a245c576e66225ef';
-    const fetchURL = `https://api.openweathermap.org/data/2.5/weather?q=${userCity}&APPID=${APIkey}`;
+  fetch(fetchURL, { mode: "cors" })
+    .then((response) => response.json())
+    .then((data) => {
+      updateDisplay(data);
+    })
+    .catch((err) => {
+      console.log(err);
+      displayError();
+    });
+}
 
-    fetch(fetchURL, { mode: 'cors' })
-      .then((response) => response.json())
-      .then((data) => {
-        updateDisplay(data);
-      })
-      .catch((err) => {
-        console.log(err);
-        updateDisplay(null);
-      });
-  }
+function updateDisplay(data) {
+  const city = data.name;
+  const summary = data.weather[0].main;
+  const present = toCelsius(data.main.temp);
+  const max = toCelsius(data.main.temp_max);
+  const min = toCelsius(data.main.temp_min);
+  const humidity = data.main.humidity;
+  const feelsLike = toCelsius(data.main.feels_like);
 
-  function updateDisplay(data) {
-    let city = '-';
-    let summary = '-';
-    let present = '-';
-    let max = '-';
-    let min = '-';
-    let humidity = '-';
-    let feelsLike = '-';
-    if (data) {
-      city = data.name;
-      summary = data.weather[0].main;
-      present = toCelsius(data.main.temp);
-      max = toCelsius(data.main.temp_max);
-      min = toCelsius(data.main.temp_min);
-      humidity = data.main.humidity;
-      feelsLike = toCelsius(data.main.feels_like);
-    }
-
-    mainDiv.innerHTML = `  <div class="city-name">${city}</div>
+  mainDiv.innerHTML = `  <div class="city-name">${city}</div>
     <div class="summary">${summary}</div>
     <div class="weather-info">
         <div class="present">Weather Now: ${present}</div>
@@ -51,10 +39,13 @@ import './style.css';
         <div class="humidity">Humidity: ${humidity}%</div>
         <div class="feels-like">Feels Like: ${feelsLike}</div>
     </div>`;
-  }
+}
 
-  function toCelsius(K) {
-    const kelvinConstant = 273.15;
-    return `${(K - kelvinConstant).toFixed(1)}&deg;C`;
-  }
-})();
+function toCelsius(K) {
+  const kelvinConstant = 273.15;
+  return `${(K - kelvinConstant).toFixed(1)}&deg;C`;
+}
+
+function displayError() {
+  mainDiv.textContent = "Error Loading...";
+}
